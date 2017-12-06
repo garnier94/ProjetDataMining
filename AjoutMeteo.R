80641b589134f61aab1c7d59550f6b86cc1694f9
Remy Garnier <garnier@crans.org>
2017-12-06 13:08:41 +0100

    Données avec Météo

--- AjoutMeteo.R
+++ AjoutMeteo.R
@@ -16,22 +16,28 @@ ConvertHour <- function(time_stamp)
 library(riem)
 library(lubridate)
 library(tidyverse)
+library(weathermetrics)
 
 minDate <- min(Data2013perHour$Day)
 maxDate <- max(Data2013perHour$Day)
 
-riem_networks()
 Meteo <- riem_measures(station = "MDW", date_start = as.character(minDate), date_end = as.character(maxDate))
-Day <- date(Meteo$valid)
-Meteo$Day <- Day
+Meteo$tmpf <- fahrenheit.to.celsius(Meteo$tmpf)
+Meteo$Day <- date(Meteo$valid)
+Meteo$Month <- month(Meteo$valid)
 
 HeureNorm <- sapply(Meteo$valid,ConvertHour)
 Meteo$Hour <- as.array(HeureNorm)
 
 sumMeteo <- summarise(group_by(Meteo, Day, Hour),temp = mean(tmpf), pluvio = mean(p01i))
-sumMeteo$temp <- (sumMeteo$temp -32)/1.8  ## Conversion Celsius
-Data2013withMeteo <- left_join(Data2013perHour,sumMeteo, by= c("Hour" ="Hour", "Day" ="Day"))
+DatawithMeteo <- left_join(Data2013perHour,sumMeteo, by= c("Hour" ="Hour", "Day" ="Day"))
 
-### éliminer les températures 
 
-Data2013withMeteo$pluvio[is.na(Data2013withMeteo$pluvio)] <-0
+### éliminer les NA
+
+DatawithMeteo$pluvio[is.na(DatawithMeteo$pluvio)] <-0 # On suppose qu'il n'y a pas de pluie
+
+MeanMonth <- summarise(group_by(Meteo,Month,Hour),temp = mean(tmpf))
+save(DatawithMeteo,file = "AggratedData2013.RData")
+
+
