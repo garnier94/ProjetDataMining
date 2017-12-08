2165bf5e3f115e9a33c5e233bdc0bc172d753da1
Camille Palmier <camillepalmier@MacBook-Pro-de-Camille.local>
2017-12-08 14:43:09 +0100

    modif jours speciaux, rajout des autres annees

--- AjoutSpecialDays.R
+++ AjoutSpecialDays.R
@@ -1,30 +1,51 @@
 load("~/Documents/Orsay/M2/Data Mining/ProjetDataMining/AggratedData2013.RData")
+Data2013perHour[is.na(Data2013perHour)] <- 0
 
 # data_frame avec les jours fériés en 2013
 # same special days with the function holidayNYSE in the package timeDate
-special = c("2013-01-01","2013-01-21","2013-02-12","2013-02-18","2013-05-27","2013-07-04","2013-09-02","2013-10-14","2013-11-11","2013-11-28","2013-11-29","2013-12-25")
+special_2013 = c("2013-01-01","2013-01-21","2013-02-12","2013-02-18","2013-05-27","2013-07-04","2013-09-02","2013-10-14","2013-11-11","2013-11-28","2013-11-29","2013-12-25")
+special_2014 = c("2014-01-01","2014-01-20","2014-02-12","2014-02-17","2014-05-26","2014-07-04","2014-09-01","2014-10-13","2014-11-11","2014-11-27","2014-11-28","2014-12-25")
+special_2015 = c("2015-01-01","2015-01-19","2015-02-12","2015-02-16","2015-05-25","2015-07-04","2015-09-07","2015-10-12","2015-11-11","2015-11-26","2015-11-27","2015-12-25")
+special_2016 = c("2016-01-01","2016-01-18","2016-02-12","2016-02-15","2016-05-30","2016-07-04","2016-09-05","2016-10-10","2016-11-11","2016-11-24","2016-11-25","2016-12-25")
+special_2017 = c("2017-01-01","2017-01-16","2017-02-12","2017-02-20","2017-05-29","2017-07-04","2017-09-04","2017-10-09","2017-11-11","2017-11-23","2017-11-24","2017-12-25")
 
-Date <- as.POSIXct(strptime(special,"%Y-%m-%d"))
-special <- Date
+Date <- as.POSIXct(strptime(special_2013,"%Y-%m-%d"))
+special_2013 <- Date
+rm(Date)
+
+Date <- as.POSIXct(strptime(special_2014,"%Y-%m-%d"))
+special_2014 <- Date
+rm(Date)
+
+Date <- as.POSIXct(strptime(special_2015,"%Y-%m-%d"))
+special_2015 <- Date
+rm(Date)
+
+Date <- as.POSIXct(strptime(special_2016,"%Y-%m-%d"))
+special_2016 <- Date
+rm(Date)
+
+Date <- as.POSIXct(strptime(special_2017,"%Y-%m-%d"))
+special_2017 <- Date
 rm(Date)
 
 type_days = c("NewYears","MLK Day","Lincoln'sBirthday","PresidentDay","MemorialDay","IndependenceDay","LaborDay","ColumbusDay", "VeteransDay","Thanksgiving","ThanksgivingFriday","ChristmasDay")
 
-special_days <- data.frame(special, type_days)
+special_days <- data.frame(type_days,special_2013,special_2014,special_2015,special_2016,special_2017)
 
 # day of the week : 
 
 Data2013perHour$dow = as.numeric(format(Data2013perHour$Day, format = "%u"))
 Data2013perHour$weekday = format(Data2013perHour$Day, format = "%a")
 
-for(i in 1:length(special)){
-  positions <- which(Data2013perHour$Day == special_days$special[i])
+for(i in 1:length(special_2013)){
+  positions <- which(Data2013perHour$Day == special_days$special_2013[i])
   Data2013perHour$dow[positions] = 8
   Data2013perHour$weekday[positions] = as.character(special_days$type_days[i])
 }
 
 # examples
-head(Data2013perHour[which(Data2013perHour$Day == special_days$special[10]),],5)
-head(Data2013perHour[which(Data2013perHour$Day == special_days$special[7]),],5)
+head(Data2013perHour[which(Data2013perHour$Day == special_days$special_2013[10]),],5)
+head(Data2013perHour[which(Data2013perHour$Day == special_days$special_2013[7]),],5)
 
 save(Data2013perHour, file="Aggregate_data_special_days.RData")
