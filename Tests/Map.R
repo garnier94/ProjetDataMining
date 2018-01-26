97422c5fee43d372e1784934973228e9246edc4a
Rémy Garnier <garnier@crans.org>
2018-01-26 13:39:40 +0100

    Rbesa

--- Tests/Map.R
+++ Tests/Map.R
@@ -1,68 +1,30 @@
+rm(list= objects())
 library(rgdal)
 library(raster)
 library(tidyverse)
 
-#Chargement de la carte:
-illinois <- readOGR(path.expand("~/statML/Projet/ProjetDataMining/Map"),"tl_2016_17_tract")
-plot(illinois)
-class(illinois)
-
-
-#View attributes:
-illinois@data
-CookCounty <- illinois[which(illinois$COUNTYFP=="031"),]
-
-
-#-----------------------------------------------------------------------------------------------
-
-stations <- readOGR(path.expand("~/statML/Projet/ProjetDataMining/data/Divvy_Stations_Trips_2014_Q3Q4/Divvy_Stations_2014_Q3Q4"), "Divvy_Stations_2015")
-
-
-plot(CookCounty)
-plot(stations, add=TRUE,col ='red')
-
 #--------------------
-Stations2017 <- read.csv("~/statML/Projet/ProjetDataMining/data/Divvy_Stations_2015.csv")
+Stations2017 <- read.csv("~/StatML/DataProjet/Divvy_Stations_2015.csv")
+station_map <- readOGR(path.expand("~/StatML/DataProjet/Divvy_Stations_Trips_2014_Q3Q4/Divvy_Stations_2014_Q3Q4"),"Divvy_Stations_2015")
 
-minLat = min(Stations2017$latitude)-0.1
-maxLat = max(Stations2017$latitude)+0.1
-minLon = min(Stations2017$longitude)-0.1
-maxLon = max(Stations2017$longitude )+0.1
 
-is_in_Border= function( census_tract)
+ecart = 0.09
+
+minLat = min(Stations2017$latitude)-ecart
+maxLat = max(Stations2017$latitude)+ecart
+minLon = min(Stations2017$longitude)-ecart
+maxLon = max(Stations2017$longitude )+ecart
+
+is_in_Border= function(polygone)
 {
-  classe = extent(census_tract)
+  classe = extent(polygone)
   return((classe@xmin > minLon ) && (classe@xmax < maxLon) && (classe@ymin > minLat ) && (classe@ymax < maxLat))
 }
 
-lon = length(CookCounty)
-liste = rep.int(FALSE,lon)
-for(i in 1:lon)
-{
-  liste[i] <- is_in_Border(CookCounty[i,])  
-}
 
-Chicago <- CookCounty[liste,]
-
-plot(Chicago)
-plot(stations, add=TRUE,col ='red')
-
-### On n'observe pas la même densité
-crs(Chicago)
-# +proj=longlat +datum=NAD83 +no_defs +ellps=GRS80 +towgs84=0,0,0 
-crs(stations)
-#+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0
-
-#On doit reprojeter 
-stationsRepro <- spTransform(stations,crs(Chicago))
-crs(stationsRepro)
-crs(Chicago)
-
-plot(Chicago)
-plot(stationsRepro, add = T, col = 'red')
-
-##Testons autre chose :
-illinoisCR <- readOGR(path.expand("~/statML/Projet/ProjetDataMining/Map/Congres"),"tl_2016_17_sldl")
+illinoisCR <- readOGR(path.expand("~/StatML/Projet/ProjetDataMining/Map/Congres"),"tl_2016_17_sldl")
+  
+  
 plot(illinoisCR)
 illinoisCR@data
 lon = length(illinoisCR)
@@ -72,4 +34,10 @@ for(i in 1:lon)
   liste[i] <- is_in_Border(illinoisCR[i,])  
 }
 ChicagoR <- illinoisCR[liste,]
-plot(ChicagoR)
+
+plot.new()
+plot(ChicagoR, axes= F, xlab ="", ylab="", add= T)
+axis(2, ylim=c(minLat, maxLat))
+axis(1, xlim=c(minLon, maxLon))
+plot(station_map, col='red')
+
