9d290bc50377be09762841f8861355cc29fecb02
Rémy Garnier <garnier@crans.org>
2018-02-02 17:23:56 +0100

    Rassemblement des données

--- Tests/Ajout_Données_Géo.R
+++ Tests/Ajout_Données_Géo.R
@@ -2,41 +2,46 @@ rm(list= objects())
 library(rgdal)
 library(raster)
 library(tidyverse)
-
-
+library(DivvyBikeProject)
+library(riem)
+library(weathermetrics)
+library(lubridate)
 
 #Importation des données géographiques 
-
 illinoisCR <- readOGR(path.expand("~/StatML/Projet/ProjetDataMining/Map/Congres"),"tl_2016_17_sldl")
 #illinoisCR <- readOGR(path.expand("~/Documents/Orsay/M2/Semestre 1/Data Mining/ProjetDataMining/Map/Congres"),"tl_2016_17_sldl")  
 
 # Importation des stations
-Stations2017 <- read.csv("~/StatML/DataProjet/Divvy_Stations_2017_Q1Q2.csv")
-Stations.map <- Stations2017
+Stations <-  read_csv("~/StatML/DataProjet/Divvy_Stations_Trips_2014_Q3Q4/Divvy_Stations_2014-Q3Q4.csv")
+Stations.map <- Stations
 
-#Trips2017:
-load("~/StatML/DataProjet/AggregatedData/Full_data2017.RData")
+#Trips2014
+load("~/StatML/Projet/ProjetDataMining/Package/AggratedData2014.RData")
 
-# Gestion des cartes
-coordinates(Stations.map) <- ~ longitude + latitude
-proj4string(Stations.map) <- proj4string(illinoisCR)
-Stations2017$district <-as.integer( over(Stations.map, illinoisCR)$SLDLST)
+year = 2014
 
-#Jointure Table
-Datat <- left_join(Data2017,Stations2017, by=c("station"="id"))
-Group  <- summarise(group_by(Datat, Day,  dow, weekday, Hour, district, temp, pluvio ), nbE = sum(nbE), nbS = sum(nbS)  ) 
-Group$diff  <- Group$nbE - Group$nbS
+Joined <- groupByGeospatialData(Data, illinoisCR, Stations.map)
+Meteo <- getMeteo(year, save_data = FALSE)
+ListDays <- getSpecialDays(year, save_data = FALSE)
+
+dis <- summarise(group_by(Joined, district))
+minDate <- as.POSIXct(strptime( paste(year, "01-01 00:00", sep = "-"), "%Y-%m-%d %H:%M"  ) )
+maxDate <- as.POSIXct(strptime( paste(year+1, "01-01 01:00", sep = "-"), "%Y-%m-%d %H:%M"  ) )
+listDate <- seq(minDate, maxDate, by = "hour")
+
+Join <- Joined[,c(1,4)]
+
+expansion <- expand(Join, Time = listDate, district )
+Data <- left_join(expansion, Joined , by = c("district","Time"))
+
+Data$Day <-  format(Data$Time, "%Y-%m-%d" )
+Data$Hour <- hour(Data$Time)
+Data$nbE[which(is.na(Data$nbE))] <- 0
+Data$nbS[which(is.na(Data$nbS))] <- 0
+Data$diff <- Data$nbE -Data$nbS
+ListDays$Day <- format( ListDays$Day, "%Y-%m-%d")
+Data <- left_join(Data, ListDays, by = "Day")
 
 
+DataFinal <- left_join(Data, Meteo, by = "Time")
 
-
-
-
-Group$timestamp <-  as.POSIXct(strptime( paste(Group$Day, Group$Hour, sep = " "),"%Y-%m-%d %H")) 
-
-
-
-
-#
-Regio = Group[which(Group$district==21 & Group$Day <= "2017-05-10" & Group$Day >= "2017-05-01"  ),]
-plot(Regio$timestamp, Regio$diff, type ='l' )
